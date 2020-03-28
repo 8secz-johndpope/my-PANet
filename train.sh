206f3a95bc03dcc34b79ed28375abcbcd7a7cc76
PB17151767 <jpq6699@mail.ustc.edu.cn>
2020-03-28 19:49:01 +0800

    new script

---
+++ train.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo y | conda install pytorch==1.0.1 torchvision==0.2.1 cudatoolkit=10.0 -c pytorch
+echo y | conda install -c conda-forge pycocotools
+pip install sacred
+pip install numpy==1.16.1
+./voc_align_1way_1shot_[train].sh
