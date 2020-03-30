ead9ccea6ec2c4fb548913681bfa69173b848b28
PB17151767 <jpq6699@mail.ustc.edu.cn>
2020-03-30 15:38:31 +0800

    change for some shell script

--- musoc_env.sh
+++ musoc_env.sh
@@ -4,3 +4,6 @@ apt-get install sudo
 echo y | sudo apt-get install openslide-tools
 echo y | sudo apt-get install python-openslide
 echo y | conda install -c bioconda openslide-python
+echo y | conda install -c conda-forge libiconv
+echo y | conda install -c conda-forge shapely
+echo y | conda install -c conda-forge opencv
